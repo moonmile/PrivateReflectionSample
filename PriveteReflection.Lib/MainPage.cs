c393f828bf87186061a00f64619d3a6f6d6778ab
Tomoaki Masuda <masuda@moonmile.net>
2014-08-01 15:12:10 +0900

    実験コード

---
+++ PriveteReflection.Lib/MainPage.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+
+namespace PriveteReflection.Lib
+{
+    public class MainPage
+    {
+        /// <summary>
+        /// 元ポインタは、dynamic で保存しておく。
+        /// こうすると、動的にプロパティ/メソッドを呼び出しができるようになる。
+        /// </summary>
+        public object Target { get; set; }
+        public dynamic text1 { get; set; }
+        public dynamic btn1 { get; set; }
+
+        public MainPage(object target)
+        {
+            // 元の MainPage をバインド
+            this.Target = target;
+
+            // private フィールドを探索する
+            text1 = FindPropNoPublic("text1");
+            btn1 = FindPropNoPublic("btn1");
+            // クリックイベントを設定する
+            Action<object, object> action = OnClickButton;
+            AddEventHandler(this.btn1, "Click", action);
+        }
+
+        private int count = 0;
+
+        private void OnClickButton(object sender, object e)
+        {
+            ++count;
+            text1.Text = string.Format("reflection : {0} clicked.", count);
+        }
+
+        /// <summary>
+        /// プライベートフィールドを探索する
+        /// XAMLで生成される button1 などのフィールドが privete で作られているので
+        /// これを見つけ出して割り当てるため
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public object FindPropNoPublic(string name)
+        {
+            Type t = Target.GetType();
+            var fi = t.GetTypeInfo().GetDeclaredField(name);
+            var obj = fi.GetValue(Target);
+            return obj;
+        }
+
+        /// <summary>
+        /// イベントを設定する
+        /// 特に Click イベントは RoutedEventArgs 設定する必要があるので、
+        /// この方式を使う
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="eventName"></param>
+        /// <param name="action"></param>
+        public void AddEventHandler(object target, string eventName, Action<object, EventArgs> action)
+        {
+            var ei = target.GetType().GetRuntimeEvent(eventName);
+            Type tt = ei.AddMethod.GetParameters()[0].ParameterType;
+            var mi = ei.AddMethod;
+            // イベントの型を変えるために一度ラムダ式でくるむ
+            Action<object, object> handler = ((s, e) => { action(s, new EventArgs()); });
+            var handlerInvoke = action.GetType().GetRuntimeMethod(
+                "Invoke", new Type[] { typeof(object), typeof(Type[]) });
+            var dele = handlerInvoke.CreateDelegate(tt, handler);
+            mi.Invoke((object)target, new object[] { dele });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,48 @@
+PrivateReflectionSample
+===============
+How to search a private field.
+How to add method of reflection event
+
+プライベートフィールドを探索する方法と、リフレクションを使ってイベントを登録する方法のサンプルコードです。
+
+リフレクションを使った View と Logic の完全分離（Logicは Portable Class Library で作成）するための実験コードから切り出し。Windows Store 8.1 と Windows Phone 8.1 で共通 PCL を使う。似た感じで、Xamarin.Forms も作れるので、Windows.UI.Xaml と Xamarin.Forms.Xaml が共通コードで扱えるようになる。
+
+# プライベートフィールドの探索
+
+GetTypeInfo().GetDeclaredField(name) で、目的のプライベートフィールを取得できる。
+
+```C#
+text1 = FindPropNoPublic("text1");
+
+public object FindPropNoPublic(string name)
+{
+    Type t = Target.GetType();
+    var fi = t.GetTypeInfo().GetDeclaredField(name);
+    var obj = fi.GetValue(Target);
+    return obj;
+}
+```
+
+# イベントの登録
+
+```C#
+Action<object, object> action = OnClickButton;
+AddEventHandler(this.btn1, "Click", action);
+
+public void AddEventHandler(object target, string eventName, Action<object, EventArgs> action)
+{
+    var ei = target.GetType().GetRuntimeEvent(eventName);
+    Type tt = ei.AddMethod.GetParameters()[0].ParameterType;
+    var mi = ei.AddMethod;
+    // イベントの型を変えるために一度ラムダ式でくるむ
+    Action<object, object> handler = ((s, e) => { action(s, new EventArgs()); });
+    var handlerInvoke = action.GetType().GetRuntimeMethod(
+        "Invoke", new Type[] { typeof(object), typeof(Type[]) });
+    var dele = handlerInvoke.CreateDelegate(tt, handler);
+    mi.Invoke((object)target, new object[] { dele });
+}
+
+# Author 
+
+Tomoaki Masuda @moonmile
+
